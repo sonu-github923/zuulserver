c6253ec120b8ef18949f099058ef001b853c295e
sonu kumar <sonuthakur1928@gmail.com>
2020-04-21 19:43:33 +0530

    first commit

---
+++ src/main/java/com/example/zuulserver/App.java
@@ -0,0 +1,23 @@
+package com.example.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
